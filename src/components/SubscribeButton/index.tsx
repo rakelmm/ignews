20b7e5a961d373d6c643310cc9dcb0951f7ed232
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-03-29 20:53:59 -0300

    Consumindo API do Stripe (SSR)

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface subscribeButtonprops {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: subscribeButtonprops) {
     return (
         <button
             type="button"
