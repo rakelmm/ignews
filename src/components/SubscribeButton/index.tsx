ff235739ac93df0b9e0a85bd6324b44e8667fb43
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-05-13 18:11:42 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,29 +1,47 @@
-import { SessionProvider } from "next-auth/react";
-import { useSession, signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
-interface subscribeButtonprops {
+interface SubscribeButtonProps {
     priceId: string;
 }
 
-export function SubscribeButton({ priceId }: subscribeButtonprops) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
-            signIn('github')
+            signIn();
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe!.redirectToCheckout({ sessionId })
+
+        } catch (err) {
+            let errorMessage = 'Failed';
+            if (err instanceof Error) {
+                errorMessage = err.message;
+            }
+            alert(errorMessage);
+        }
     }
 
     return (
         <button
-            type="button"
+            type='button'
             className={styles.subscribeButton}
             onClick={handleSubscribe}
         >
             Subscribe now
         </button>
-    );
+
+    )
 }
