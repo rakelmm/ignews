7b86e9728bfeb3185ad8d9765df30748c6413cfd
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-05-18 16:49:30 -0300

    Navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -7,8 +9,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                <a className={styles.active} href="">Home</a>
-                <a href="">Posts</a>
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts">
+                        <a >Posts</a>
+                    </Link>
                 </nav>
 
                 <SignInButton />
